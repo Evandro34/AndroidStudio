9150cb5e536b7106e4fd141c89092a5a9233be5c
Evandro34 <evancastro31@hotmail.com>
2024-11-25 19:04:51 -0300

    criando LOG

--- app/src/main/java/com/evancastro29/classelivro/MainActivity.java
+++ app/src/main/java/com/evancastro29/classelivro/MainActivity.java
@@ -3,6 +3,7 @@ package com.evancastro29.classelivro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
 
         Livro objLivro = new Livro();
         objLivro.setAutor("Marco Maddo");
+        objLivro.setTitulo("POO Encaspsulamento Seguro em Java");
+        objLivro.setEditora("Mundial POO");
+        objLivro.setPaginas(140);
+        objLivro.setLocalizacao("L1L1");
+
+        Log.i("POO","Obj Livro: Autor" + objLivro.getAutor());
+        Log.i("POO","Obj Livro: Titulo" + objLivro.getTitulo());
+        Log.i("POO","Obj Livro: Editora" + objLivro.getEditora());
+        Log.i("POO","Obj Livro: Pagina" + objLivro.getPaginas());
+        Log.i("POO","Obj Livro: Localizacao" + objLivro.getLocalizacao());
+
 
     }
 }
